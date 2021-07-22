c924d857ae35579f82e5e36d070532ef434e2f77
Shi zhen Lim <slim@u81z01s01.42kl.edu.my>
2021-07-22 20:53:45 +0800

    1

--- libft.h
+++ libft.h
@@ -2,6 +2,7 @@
 # define LIBFT_H
 
 # include <stdio.h>
+# include <stdlib.h>
 
 int				ft_memcmp(const void *s1, const void *s2, size_t n);
 int				ft_atoi(const char *str);
@@ -14,12 +15,12 @@ int				ft_strncmp(const char *s1, const char *s2, size_t n);
 int				ft_tolower(int c);
 int				ft_toupper(int c);
 
-void			bzero(void *s, size_t n);
+void			ft_bzero(void *s, size_t n);
 void			*ft_memccpy(void *dst, const void *src, int c, size_t n);
 void			*ft_memchr(const void *s, int c, size_t n);
-void			*ft_memcpy(void *dest, const void *src, size_t n);
+void			*ft_memcpy(void *dst, const void *src, size_t n);
 void			*ft_memmove(void *dst, const void *src, size_t len);
-void			*memset(void *b, int c, size_t len);
+void			*ft_memset(void *b, int c, size_t len);
 
 char			*ft_strchr(const char *str, int c);
 char			*ft_strnstr(const char *haystack,
